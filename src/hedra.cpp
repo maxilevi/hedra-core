aa87a54a9e993baa0d09c16a232e68623f1ab581
Zaphyk <zaphyk@live.com>
2019-09-11 00:05:44 -0300

    added some functions for managing fast noise

--- src/hedra.cpp
+++ src/hedra.cpp
@@ -2,3 +2,32 @@
  * Created by Maximiliano Levi on 10/09/2019.
  */
 
+#include "hedra.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+HEDRA_API FastNoiseSIMD* fastnoise_createObject()
+{
+    return FastNoiseSIMD::NewFastNoiseSIMD();
+}
+
+HEDRA_API void fastnoise_deleteObject(FastNoiseSIMD* pointer)
+{
+    delete (pointer);
+}
+
+HEDRA_API void fastnoise_freeNoise(float* noiseSet)
+{
+    FastNoiseSIMD::FreeNoiseSet(noiseSet);
+}
+
+HEDRA_API float* fastnoise_getSimplexFractalSetWithFrequency(FastNoiseSIMD* pointer, int xStart, int yStart, int zStart, int xSize, int ySize, int zSize, float scaleModifier)
+{
+    return pointer->GetSimplexFractalSet(xStart, yStart, zStart, xSize, ySize, zSize, scaleModifier);
+}
+
+#ifdef __cplusplus
+}
+#endif
--- src/hedra.h
+++ src/hedra.h
@@ -2,7 +2,22 @@
  * Created by Maximiliano Levi on 10/09/2019.
  */
 
-#ifndef HEDRACORE_HEDRA_H
-#define HEDRACORE_HEDRA_H
+#include "fastnoise-simd/FastNoiseSIMD.h"
+#define HEDRA_API __declspec(dllexport)
 
-#endif //HEDRACORE_HEDRA_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    HEDRA_API FastNoiseSIMD* fastnoise_createObject();
+
+    HEDRA_API float* fastnoise_getSimplexFractalSetWithFrequency(FastNoiseSIMD* pointer, int xStart, int yStart, int zStart, int xSize, int ySize, int zSize, float scaleModifier);
+
+    HEDRA_API void fastnoise_deleteObject(FastNoiseSIMD* pointer);
+
+    HEDRA_API void fastnoise_freeNoise(float* pointer);
+
+
+#ifdef __cplusplus
+}
+#endif
