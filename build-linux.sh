66f2304c7ffd95057b56a102da15271e45ee1dc0
Maximiliano Levi <maxilevi77@gmail.com>
2021-11-16 13:06:03 -0300

    fix linux build script for both archs

--- build-linux.sh
+++ build-linux.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-rm -r -dir --force ./cmake-build-x64-linux/
-mkdir ./cmake-build-x64-linux/
-cd ./cmake-build-x64-linux/
-cmake ./..
-cmake --build ./..
+echo $PWD
 
 rm -r -dir --force ./cmake-build-x32-linux/
 mkdir ./cmake-build-x32-linux/
 cd ./cmake-build-x32-linux/
-cmake ./..
-cmake --build ./..
+cmake ./../
+cmake --build .
+
+cd ./../
+
+rm -r -dir --force ./cmake-build-x64-linux/
+mkdir ./cmake-build-x64-linux/
+cd ./cmake-build-x64-linux/
+cmake ./../
+cmake --build .
